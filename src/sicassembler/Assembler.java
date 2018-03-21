fdb6473197b1717da6d672d56e4b6583382352a1
Ahmed Nawar <ahmednawar2007@gmail.com>
2018-03-21 21:41:18 +0200

    -fixed locationCounter

--- src/sicassembler/Assembler.java
+++ src/sicassembler/Assembler.java
@@ -72,75 +72,89 @@ public class Assembler {
         }
 
     }
-    
-    public void executePassTwo(){
-        System.out.println(symTable.get("RETADR"));
+
+    public void executePassTwo() {
         OpTable opCodes = new OpTable();
-        writer=new StringBuilder();
-        writer.append("H").append(programName).append(" ") 
-                .append(Integer.toHexString(startingAddress)).append(formatHexa(Integer.toHexString(programLength),4));
-        int t =30;
-        for(Instruction instruction : instructions ){
-            if(t==30){
-                t=0;
+        writer = new StringBuilder();
+        writer.append("H").append(programName).append(" ")
+                .append(formatHexa(Integer.toHexString(startingAddress), 6)).append(" ").append(formatHexa(Integer.toHexString(programLength), 6));
+        int t = 30;
+        for (Instruction instruction : instructions) {
+            if (t == 30) {
+                t = 0;
                 writer.append("\n");
                 writer.append("T").append(Integer.toHexString(instruction.getLocation()));
                 if (instructions.get(instructions.size() - 1).getLocation() > instruction.getLocation() + 30) {
                     writer.append("1E");
-                } else if(instructions.get(instructions.size()-1).getLocation() > instruction.getLocation() +15){
-                    writer.append(Integer.toHexString(instructions.get(instructions.size()-1).getLocation()-instruction.getLocation()));
-                }
-                else{
+                } else if (instructions.get(instructions.size() - 1).getLocation() > instruction.getLocation() + 15) {
+                    writer.append(Integer.toHexString(instructions.get(instructions.size() - 1).getLocation() - instruction.getLocation()));
+                } else {
                     writer.append(0);
-                    writer.append(Integer.toHexString(instructions.get(instructions.size()-1).getLocation()-instruction.getLocation()));   
+                    writer.append(Integer.toHexString(instructions.get(instructions.size() - 1).getLocation() - instruction.getLocation()));
                 }
             }
-            System.out.println(((instruction.getSymbol() == null)? "\t\t":instruction.getSymbol()+"\t")+instruction.getMnemonic()+"\t"+instruction.getOperand());
-            if(opCodes.getOpCode(instruction.getMnemonic())!=null){
+            System.out.println(((instruction.getSymbol() == null) ? "\t\t" : instruction.getSymbol() + "\t") + instruction.getMnemonic() + "\t" + instruction.getOperand());
+            if (opCodes.getOpCode(instruction.getMnemonic()) != null) {
                 writer.append(opCodes.getOpCode(instruction.getMnemonic()));
                 t++;
             }
-            
-            if(instruction.getOperand() == null) {
+
+            if (instruction.getOperand() == null) {
                 writer.append(0000);
-            }
-            else if(instruction.getOperand().charAt(0)>= '0' && instruction.getOperand().charAt(0)<= '9' ){
+            } else if (instruction.getOperand().charAt(0) >= '0' && instruction.getOperand().charAt(0) <= '9') {
                 String hexaOperand = Integer.toHexString(Integer.parseInt(instruction.getOperand()));
                 writer.append(formatHexa(hexaOperand, 6));
                 t++;
-            }
-            else if(symTable.get(instruction.getOperand())!=null){
-               writer.append(formatHexa(Integer.toHexString(symTable.get(instruction.getOperand())),4));
-            }
-            else if(instruction.getOperand().substring(0, 2).equalsIgnoreCase("x'") 
-                    && instruction.getOperand().charAt(instruction.getOperand().length()-1)=='\''){
-                writer.append(instruction.getOperand().substring(2,instruction.getOperand().length()-1));
-            }
-            else if(instruction.getOperand().substring(0, 2).equalsIgnoreCase("c'") 
-                    && instruction.getOperand().charAt(instruction.getOperand().length()-1)=='\''){
-                for(char c : instruction.getOperand().substring(2,instruction.getOperand().length()-1).toCharArray()){
+            } else if (symTable.get(instruction.getOperand()) != null) {
+                writer.append(formatHexa(Integer.toHexString(symTable.get(instruction.getOperand())), 4));
+            } else if (instruction.getOperand().substring(0, 2).equalsIgnoreCase("x'")
+                    && instruction.getOperand().charAt(instruction.getOperand().length() - 1) == '\'') {
+                writer.append(instruction.getOperand().substring(2, instruction.getOperand().length() - 1));
+            } else if (instruction.getOperand().substring(0, 2).equalsIgnoreCase("c'")
+                    && instruction.getOperand().charAt(instruction.getOperand().length() - 1) == '\'') {
+                for (char c : instruction.getOperand().substring(2, instruction.getOperand().length() - 1).toCharArray()) {
                     int ascii = (char) c;
                     writer.append(Integer.toHexString(ascii));
                 }
-            }
-            else{
+            } else {
 //                JOptionPane.showMessageDialog(new JFrame(), 
 //                        "Assembling Error: Unknown Operand \"" + instruction.getOperand(), "Dialog",
 //                JOptionPane.ERROR_MESSAGE);
                 writer.append("0000"); // to be removed
             }
-            t+=2;
+            t += 2;
         }
         writer.append("\n");
         writer.append("E").append(Integer.toHexString(startingAddress));
     }
 
     public void writeFile(String filePath) {
-
+        ArrayList<String> objectCodes = new ArrayList<>();
         try {
-            FileWriter filewriter = new FileWriter(filePath);
+            FileWriter filewriter = new FileWriter("objectCode.txt");
             BufferedWriter writer = new BufferedWriter(filewriter);
-            writer.write("hi");
+
+            writer.append("H").append(programName).append(" ")
+                    .append(formatHexa(Integer.toHexString(startingAddress), 6)).append(" ")
+                    .append(formatHexa(Integer.toHexString(programLength), 6));
+            writer.newLine();
+            System.out.println("size of T 0: " + getSizeOfT(0));
+            writer.append("T ").append(Integer.toHexString(instructions.get(0).getLocation())).append(" ").append(getSizeOfT(0));
+            int objectsCount = 0;
+            for (int i = 0; i < instructions.size(); i++) {
+                Instruction instruction = instructions.get(i);
+                if (objectsCount++ < 10) {
+                    writer.append(instruction.getMnemonic());
+                    if (instruction.getOperand() != null || symTable.containsKey(instruction.getOperand())) {
+                        writer.append(Integer.toHexString(symTable.get(instruction.getOperand())));
+                    }
+                } else {
+                    writer.newLine();
+                    writer.append("T ");
+                    continue;
+                }
+            }
+
             writer.close();
         } catch (IOException ex) {
             Logger.getLogger(Assembler.class.getName()).log(Level.SEVERE, null, ex);
@@ -158,9 +172,9 @@ public class Assembler {
         Matcher m2 = r2.matcher(line);
         Pattern r3 = Pattern.compile(pattern3);
         Matcher m3 = r3.matcher(line);
-      if (m1.find()||m2.find()||m3.find()) {
-          return true;
-      }
+        if (m1.find() || m2.find() || m3.find()) {
+            return true;
+        }
         System.out.println(line);
         System.out.println("Assembly line is incorrect");
         throw new RuntimeException("ERROR, Assembly instruction is incorrect");
@@ -171,6 +185,26 @@ public class Assembler {
             return true;
         }
         return false;
+    }
+
+    private String getSizeOfT(int startIndex) {
+        int count = 0;
+        for (int i = startIndex; i < instructions.size(); i++) {
+
+            if ("RESW".equals(instructions.get(i).getMnemonic()) || "RESB".equals(instructions.get(i).getMnemonic())) {
+                if (count <= 10) {
+                    return Integer.toHexString(count);
+                } else {
+                    return "1E";
+                }
+            }
+            count++;
+        }
+        if (count <= 10) {
+            return Integer.toHexString(count);
+        } else {
+            return "1E";
+        }
     }
 
     private Instruction parseLine(String line) {
@@ -250,8 +284,8 @@ public class Assembler {
     private boolean parseFirstLine(String line) {
         String[] tokens = line.trim().split("\\s+");
         if (tokens.length == 3 && "START".equals(tokens[1].toUpperCase())) {
-            startingAddress = Integer.parseInt(tokens[2],16);
-            locationCounter = Integer.parseInt(tokens[2],16);
+            startingAddress = Integer.parseInt(tokens[2], 16);
+            locationCounter = Integer.parseInt(tokens[2], 16);
             programName = tokens[0].trim().toUpperCase();
             return true;
         }
@@ -269,16 +303,18 @@ public class Assembler {
 
         } else if (modifiedOperand.toUpperCase().startsWith("X")) {
             String substring = modifiedOperand.substring(2, modifiedOperand.length() - 1);
-            locationCounter += substring.length() * 4;
+            locationCounter++;
 
         } else {
             throw new RuntimeException("ERROR, Wrong format");
         }
     }
-    
-    private String formatHexa(String hexa,int length){
-        String out ="";
-        for(int i =0;i< length - hexa.length();i++){out+="0";}
-        return out+hexa;
+
+    private String formatHexa(String hexa, int length) {
+        String out = "";
+        for (int i = 0; i < length - hexa.length(); i++) {
+            out += "0";
+        }
+        return out + hexa;
     }
 }
